944b14c7f38803dc7e7117edc689043ea1419d44
igjit <igjit1@gmail.com>
2019-07-13 05:47:21 +0900

    Show selected image

---
+++ app.R
@@ -0,0 +1,18 @@
+library(shiny)
+
+sample_images <- c("parrots", "hubble", "birds", "coins")
+
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("image_name", "image", sample_images)),
+    mainPanel(
+      plotOutput(outputId = "distPlot"))))
+
+server <- function(input, output) {
+  output$distPlot <- renderPlot({
+    plot(imager::load.example(input$image_name))
+  })
+}
+
+shinyApp(ui, server)
