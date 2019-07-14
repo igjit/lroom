ca2c9a3bcfe5d91bc21d52c70ddedcaa4622dd62
igjit <igjit1@gmail.com>
2019-07-14 16:38:36 +0900

    Render image directly

--- app.R
+++ app.R
@@ -8,12 +8,19 @@ ui <- fluidPage(
     sidebarPanel(
       selectInput("image_name", "image", sample_images)),
     mainPanel(
-      plotOutput(outputId = "distPlot"))))
+      imageOutput(outputId = "dist_image"))))
 
 server <- function(input, output) {
-  output$distPlot <- renderPlot({
-    plot(load.example(input$image_name))
-  })
+  output$dist_image <- renderImage({
+    outfile <- tempfile(fileext = ".png")
+    image <- load.example(input$image_name)
+    save.image(image, outfile)
+
+    list(src = outfile,
+         contentType = "image/png",
+         width = nrow(image),
+         height = ncol(image))
+  }, deleteFile = TRUE)
 }
 
 shinyApp(ui, server)
