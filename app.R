9d91adc469db14abbc38c86b6873b7d81b0a49fb
igjit <igjit1@gmail.com>
2019-07-17 13:11:29 +0900

    Render image as plot

--- app.R
+++ app.R
@@ -3,29 +3,17 @@ library(imager)
 
 sample_images <- c("parrots", "hubble", "birds", "coins")
 
-load_as_file <- function(name) {
-  outfile <- tempfile(fileext = ".png")
-  image <- load.example(name)
-  save.image(image, outfile)
-  outfile
-}
-
 ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       selectInput("image_name", "image", sample_images)),
     mainPanel(
-      imageOutput(outputId = "dist_image"))))
+      plotOutput(outputId = "dist_image"))))
 
 server <- function(input, output) {
-  output$dist_image <- renderImage({
-    image_file <- load_as_file(input$image_name)
-
-    list(src = image_file,
-         contentType = "image/png",
-         width = nrow(image),
-         height = ncol(image))
-  }, deleteFile = TRUE)
+  output$dist_image <- renderPlot({
+    plot(load.example(input$image_name))
+  })
 }
 
 shinyApp(ui, server)
