4ba2a28d27a7d20b2481f86f3314a07e1ce96034
igjit <igjit1@gmail.com>
2019-07-18 15:24:59 +0900

    Plot histogram

--- app.R
+++ app.R
@@ -1,11 +1,14 @@
 library(shiny)
 library(imager)
+library(dplyr)
+library(ggplot2)
 
 sample_images <- c("parrots", "hubble", "birds", "coins")
 
 ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
+      plotOutput(outputId = "histogram"),
       selectInput("image_name", "image", sample_images)),
     mainPanel(
       plotOutput(outputId = "dist_image")),
@@ -15,6 +18,14 @@ server <- function(input, output) {
   image <- reactive(load.example(input$image_name))
   output$dist_image <- renderPlot({
     plot(as.raster(image()))
+  })
+  output$histogram <- renderPlot({
+    color_df <- image() %>%
+      as.data.frame %>%
+      mutate(color = c("r", "g", "b")[cc])
+    ggplot(color_df, aes(x = value, fill = color)) +
+      geom_histogram(position = "identity", alpha = 0.5) +
+      scale_fill_manual(values = c(r = "red", g = "green", b = "blue"))
   })
 }
 
