c3b68cf3cc0e46bbf29fb23e18a9e3e20721f5a9
igjit <igjit1@gmail.com>
2019-08-11 11:12:07 +0900

    Extract function to file

--- R/app_server.R
+++ R/app_server.R
@@ -1,13 +1,3 @@
-reduce_pixel <- function(image, max_width) {
-  w <- width(image)
-  h <- height(image)
-  if (w <= max_width) {
-    image
-  } else {
-    resize(image, max_width, max_width * h / w)
-  }
-}
-
 #' @import shiny
 app_server <- function(input, output, session) {
   image <- reactive(load.example(input$image_name))
