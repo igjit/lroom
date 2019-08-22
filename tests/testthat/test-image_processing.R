ef73530ea8ddcb83934f5de7b91cb9c0690352cc
igjit <igjit1@gmail.com>
2019-08-22 13:45:52 +0900

    Test apply_luminance

--- tests/testthat/test-image_processing.R
+++ tests/testthat/test-image_processing.R
@@ -5,3 +5,10 @@ test_that("reduce_pixel", {
   expect_equal(dim(reduce_pixel(image, 100)), c(100, 150, 1, 3))
   expect_equal(dim(reduce_pixel(image, 1000)), c(256, 384, 1, 3))
 })
+
+test_that("apply_luminance", {
+  image <- imager::boats
+  luminance_before <- image %>% as.vector %>% mean
+  expect_gt(apply_luminance(image, 0.5) %>% as.vector %>% mean, luminance_before)
+  expect_lt(apply_luminance(image, -0.5) %>% as.vector %>% mean, luminance_before)
+})
