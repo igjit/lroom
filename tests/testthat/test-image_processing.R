c06b49cfdcaa5d2222de7c7d76db2e7ca2f9bbd6
igjit <igjit1@gmail.com>
2019-08-23 19:32:57 +0900

    Test apply_tone_curve

--- tests/testthat/test-image_processing.R
+++ tests/testthat/test-image_processing.R
@@ -6,8 +6,18 @@ test_that("reduce_pixel", {
   expect_equal(dim(reduce_pixel(image, 1000)), c(256, 384, 1, 3))
 })
 
+luminance <- . %>% as.vector %>% mean
+
+test_that("apply_tone_curve", {
+  image <- imager::boats
+  luminance_before <- image %>% luminance
+  expect_gt(apply_tone_curve(image, splinefun(c(0, 0.5, 1), c(0, 0.6, 1))) %>% luminance,
+            luminance_before)
+  expect_lt(apply_tone_curve(image, splinefun(c(0, 0.5, 1), c(0, 0.4, 1))) %>% luminance,
+            luminance_before)
+})
+
 test_that("apply_luminance", {
-  luminance <- . %>% as.vector %>% mean
   image <- imager::boats
   luminance_before <- image %>% luminance
   expect_gt(apply_luminance(image, 0.5) %>% luminance, luminance_before)
