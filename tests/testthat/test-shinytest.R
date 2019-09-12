e74124572ec232255be409fc9c74fdcb7c12670d
igjit <igjit1@gmail.com>
2019-09-12 18:19:49 +0900

    Test luminance slider

--- tests/testthat/test-shinytest.R
+++ tests/testthat/test-shinytest.R
@@ -6,6 +6,7 @@ app <- ShinyDriver$new(".")
 
 test_that("contrast_ui works", {
   expectUpdate(app, "contrast_ui_1-contrast" = 1, output = "image_ui_1-image")
+  expectUpdate(app, "luminance_ui_1-luminance" = 1, output = "image_ui_1-image")
 })
 
 app$stop()
