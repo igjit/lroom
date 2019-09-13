1d6ed6ee3ed1a9220da21c91f86f1931813514de
igjit <igjit1@gmail.com>
2019-09-13 18:48:15 +0900

    Check if histogram is changed too

--- tests/testthat/test-shinytest.R
+++ tests/testthat/test-shinytest.R
@@ -5,8 +5,9 @@ skip_on_covr()
 app <- ShinyDriver$new(".")
 
 test_that("contrast_ui works", {
-  expectUpdate(app, "contrast_ui_1-contrast" = 1, output = "image_ui_1-image")
-  expectUpdate(app, "luminance_ui_1-luminance" = 1, output = "image_ui_1-image")
+  output <- c("image_ui_1-image", "histogram_ui_1-histogram")
+  expectUpdate(app, "contrast_ui_1-contrast" = 1, output = output)
+  expectUpdate(app, "luminance_ui_1-luminance" = 1, output = output)
 })
 
 app$stop()
